3b16c9f1b06c2c1b31a39f064ca1aeed30594054
Matthew Hyun <mattyihyun@gmail.com>
2020-09-30 16:13:33 -0700

    beginning php code and tips

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = 'Matthew Hyun';
+//In PHP the Dollar Sign, $, is used to distinguish a variable 
+
+$greeting = 'Hello my name is';
+
+echo "{$greeting} {$name}";
+
+//In PHP echo is used kind of like return or print, this is whats used to display the data on the page.
+

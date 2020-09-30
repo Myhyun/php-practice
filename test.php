1194de48b25edc89b799c11301c6a2a37e6204c8
Matthew Hyun <mattyihyun@gmail.com>
2020-09-30 16:17:01 -0700

    added more examples and comments

--- test.php
+++ test.php
@@ -7,5 +7,11 @@ $greeting = 'Hello my name is';
 
 echo "{$greeting} {$name}";
 
+echo $greeting . ',' . $name;
+
+//There are multiple ways to use variables
+//The first example shows nesting the variable within a string
+//The second example shows the use of concatenation
+
 //In PHP echo is used kind of like return or print, this is whats used to display the data on the page.
 

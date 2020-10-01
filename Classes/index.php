89cc352b882967c909d3c4bf66f68bcca2540113
Matthew Hyun <mattyihyun@gmail.com>
2020-10-01 16:58:37 -0700

    classes and arrays

--- Classes/index.php
+++ Classes/index.php
@@ -1,11 +1,11 @@
 <?php
 
-require 'functions.php';
+// require 'functions.php';
 
 class Task {
 
-    protected $description;
-    protected $completed = false;
+    public $description;
+    public $completed = false;
     //protected means that the property is shut out from the outside world, ie outside of this class
     public function __construct($description)
     {
@@ -13,11 +13,30 @@ class Task {
         //setting a property description to THIS object using the arrow
         //Automatically triggered on instantiation
     }
+
+    public function complete() {
+        $this->completed = true;
+    }
+
+    public function isComplete() {
+        return $this->completed;
+    }
 }
 
-$task = new Task('Go to the Store');
+// $task = new Task('Go to the Store'); //a new task object
+// $task->complete(); //completed the task
+// var_dump($task->isComplete()); 
 
-dd($task);
+$task =[
+    new Task ('Go to the store'),
+    new Task ('Finish my homework'),
+    new Task ('Clean my room')
+];
+
+$tasks[0]->complete();
+
+
+// dd($task);
 
 require 'index.view.php';
 
--- Classes/index.view.php
+++ Classes/index.view.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-        echo $task;
-    ?>
+    <ul>
+        <?php foreach($task as $task) : ?>
+            <li>
+                <?php if ($task->completed) : ?>
+                    <strikethrough><?= $task->description; ?></strikethrough>
+                <?php else: ?>
+                    <?= $task->description; ?>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>        
+    </ul>
 </body>
+
 </html>
